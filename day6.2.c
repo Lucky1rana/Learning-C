a4fa2b877046669d40e98c6e64b56772d52c59b7
Lucky Rana <luckyranaluckyrana995@gmail.com>
2025-08-26 11:52:33 +0530

    Add files via upload

---
+++ day6.2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int a ;
+	printf("please enter the integer : ");
+	scanf("%d",&a);
+	if(a>0)
+	{
+	
+	printf("integer is positive ",a);
+    }
+    else
+    {
+    	if(a<0){
+		
+    	printf("integer is negative ",a);
+		}
+		else
+		printf("integer is 0 ",a);
+		
+
+	}
+	
+	
+}
