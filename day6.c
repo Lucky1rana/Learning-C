bb5a00387aea21ae850b1455b65bc2f2353f8583
Lucky Rana <luckyranaluckyrana995@gmail.com>
2025-08-26 11:53:06 +0530

    Add files via upload

---
+++ day6.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a ;
+	printf("please enter the integer : ");
+	scanf("%d",&a);
+	if(a%2==0)
+	{
+		printf("integer is even ",a);
+	
+	}
+	else
+	{
+		printf("integer is odd ",a);
+	}
+}
+	
+	
