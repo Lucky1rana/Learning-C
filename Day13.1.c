c4cf8785ab0387dcd70995f35081bfcc511f0432
Lucky Rana <luckyranaluckyrana995@gmail.com>
2025-09-02 21:43:04 +0530

    Add files via upload

---
+++ Day13.1.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,result;
+    char op;
+    printf("Enter the two number and ab operator (+ , / , - , * , %) : ");
+    scanf("%d %d %c",&a,&b,&op);
+     
+     switch(op)
+     {
+     	
+     	case'+':
+     	result=a+b;
+     	printf("%d\n",result);
+     	break;
+     		
+     	case'-':
+     	result=a-b;
+     	printf("%d\n",result);
+     	break;
+     	
+     	case'*':
+     	result=a*b;
+     	printf("%d\n",result);
+     	break;
+     	
+     	case'/':
+     	if(b!=0)
+     	{
+		 
+     	result=a/b;
+     	printf("%d\n",result);
+        }
+     	else
+     	printf("error : division by zero\n");
+     	break;
+     	
+     	case'%':
+     	if(b!=0)
+     	{
+		 
+     	result=a%b;
+     	printf("%d\n",result);
+        }
+     	else
+     	printf("error : modulo by zero\n");
+     	break;
+     	
+     	default:
+     		printf("invalid operator\n");
+     		
+	 }
+	 return 0;
+}
