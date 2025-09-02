6235d0f3ab4f5b5793fa4c08bcd072f7bc7f1d92
Lucky Rana <luckyranaluckyrana995@gmail.com>
2025-09-02 10:21:20 +0530

    Update Day12.2.c

--- Day12.2.c
+++ Day12.2.c
@@ -11,16 +11,16 @@ int main()
 	if(units<=100)
 	{
 		bill=units*5;
-		printf("the bill is Rs%d",bill);
+		printf("the bill is ₹%d",bill);
 	}
 	else if(units>100&&units<=200)
 	{
 		bill=(100*5)+(units-100)*7+100;
-		printf("the bill is Rs%d",bill);
+		printf("the bill is ₹%d",bill);
 	}
 	else 
 	{ bill=(100*5)+(100*7)+(units-200)*10+500;
-		printf("the bill is Rs%d",bill);
+		printf("the bill is ₹%d",bill);
 	}
 	return 0;
 }
