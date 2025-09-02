ca267f203bb0237cdb988994416b76dc757caa76
Vansh Kumar <sji694496@gmail.com>
2025-09-02 21:43:04 +0530

    Add files via upload

---
+++ Day13.2.c
@@ -0,0 +1,22 @@
+/*Write a program to print numbers from 1 to n.*/
+
+
+
+
+#include<stdio.h>
+int main()
+{
+	int n , i;
+	printf("Enter the number : ");
+	scanf("%d",&n);
+	for (i=1;i<=n;i++)
+	{
+		printf("%d",i);
+		if(i<n)
+		{
+			printf(" ");
+		}
+	}
+	return 0;
+	
+}
