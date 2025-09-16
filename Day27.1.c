ff55af5e489f87a431137ea0f03549ea311e42a0
Vansh Kumar <sji694496@gmail.com>
2025-09-16 16:25:48 +0530

    Add files via upload

---
+++ Day27.1.c
@@ -0,0 +1,73 @@
+/*Write a program to print the following pattern:
+*
+***
+*****
+*******
+*********
+*******
+*****
+***
+* */
+
+#include<stdio.h>
+int main()
+{
+	int i;
+		for(i=0;i<1;i++)
+	{
+	printf("*");
+	}
+	printf("\n");
+	 
+	for(i=0;i<3;i++)
+	{
+		printf("*");
+	}
+	printf("\n");
+	
+	for(i=0;i<5;i++)
+	{
+		printf("*");
+	}
+	printf("\n");
+	
+	for(i=0;i<7;i++)
+	{
+		printf("*");
+	}
+	printf("\n");
+	 
+	for(i=0;i<9;i++)
+	{	
+	printf("*");
+    }
+    printf("\n");
+    
+    for(i=0;i<7;i++)
+	{
+		printf("*");
+	}
+	printf("\n");
+	
+	for(i=0;i<5;i++)
+	{
+		printf("*");
+	}
+	printf("\n");
+	
+	for(i=0;i<3;i++)
+	{
+		printf("*");
+	}
+	printf("\n");
+	 
+	for(i=0;i<1;i++)
+	{	
+	printf("*");
+    }
+    printf("\n");
+    
+    return 0;
+}
+	
+
