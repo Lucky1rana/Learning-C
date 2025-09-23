cd9f522a98e4a289f5b0ecae659a57f0de635516
Vansh Kumar <sji694496@gmail.com>
2025-09-23 18:42:30 +0530

    Add files via upload

---
+++ Day34.2.c
@@ -0,0 +1,51 @@
+/*Delete an element from an array.*/
+
+
+#include <stdio.h>
+
+int main() 
+{
+    int arr[100]; 
+    int n, pos, i;
+
+    
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) 
+	{
+        scanf("%d", &arr[i]);
+    }
+
+    
+    printf("Enter the position (1 to %d) to delete: ", n);
+    scanf("%d", &pos);
+
+    
+    if (pos < 1 || pos > n) 
+	{
+        printf("Invalid position!\n");
+    } else 
+	{
+        
+        for (i = pos - 1; i < n - 1; i++) 
+		{
+            arr[i] = arr[i + 1];
+        }
+
+        
+        n--;
+
+        
+        printf("Array after deletion:\n");
+        for (i = 0; i < n; i++) 
+		{
+            printf("%d ", arr[i]);
+        }
+    }
+
+    return 0;
+}
+
