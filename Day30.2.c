261121070cf115e541696c57e15d193634695201
Lucky Rana <luckyranaluckyrana995@gmail.com>
2025-09-26 11:58:34 +0530

    Update Day30.2.c

--- Day30.2.c
+++ Day30.2.c
@@ -3,23 +3,25 @@
 
 #include <stdio.h>
 
-int main() {
+int main() 
+{
     int n, i;
     int positive = 0, negative = 0, zero = 0;
 
     printf("Enter the number of elements: ");
     scanf("%d", &n);
 
-    int arr[n];   // 1D array
-
-    // Input elements
+    int arr[n];   
+    
     printf("Enter %d elements:\n", n);
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+    {
         scanf("%d", &arr[i]);
     }
 
-    // Count positive, negative and zero elements
-    for (i = 0; i < n; i++) {
+    
+    for (i = 0; i < n; i++) 
+    {
         if (arr[i] > 0)
             positive++;
         else if (arr[i] < 0)
@@ -28,11 +30,12 @@ int main() {
             zero++;
     }
 
-    // Output results
+   
     printf("Total positive numbers = %d\n", positive);
     printf("Total negative numbers = %d\n", negative);
     printf("Total zeros = %d\n", zero);
 
     return 0;
 }
+
 
