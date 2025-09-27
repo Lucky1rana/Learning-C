83dc1d6842b17ba406bc7ee724e09aea9566eecd
Lucky Rana <luckyranaluckyrana995@gmail.com>
2025-09-27 09:29:53 +0530

    Add files via upload

---
+++ Day38.1.c
@@ -0,0 +1,61 @@
+/*Add two matrices.*/
+
+
+#include <stdio.h>
+
+int main() 
+{
+    int r, c;  
+    int i, j;
+
+   
+    printf("Enter number of rows: ");
+    scanf("%d", &r);
+    printf("Enter number of columns: ");
+    scanf("%d", &c);
+
+    int A[r][c], B[r][c], Sum[r][c];
+
+    
+    printf("\nEnter elements of first matrix (A):\n");
+    for (i = 0; i < r; i++) 
+	{
+        for (j = 0; j < c; j++) 
+		{
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    
+    printf("\nEnter elements of second matrix (B):\n");
+    for (i = 0; i < r; i++) 
+	{
+        for (j = 0; j < c; j++) 
+		{
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+   
+    for (i = 0; i < r; i++) 
+	{
+        for (j = 0; j < c; j++) 
+		{
+            Sum[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+   
+    printf("\nSum of the two matrices:\n");
+    for (i = 0; i < r; i++) 
+	{
+        for (j = 0; j < c; j++) 
+		{
+            printf("%d\t", Sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
---
+++ Day38.2.c
@@ -0,0 +1,59 @@
+/*Check if a matrix is symmetric.*/
+
+
+#include <stdio.h>
+
+int main() 
+{
+    int r, c, i, j;
+    int flag = 1; 
+
+    
+    printf("Enter number of rows: ");
+    scanf("%d", &r);
+    printf("Enter number of columns: ");
+    scanf("%d", &c);
+
+    
+    if (r != c) 
+	{
+        printf("\nMatrix is not symmetric (not a square matrix).\n");
+        return 0;
+    }
+
+    int A[r][c];
+
+   
+    printf("Enter elements of the matrix:\n");
+    for (i = 0; i < r; i++) 
+	{
+        for (j = 0; j < c; j++) 
+		{
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    
+    for (i = 0; i < r; i++) 
+	{
+        for (j = 0; j < c; j++) 
+		{
+            if (A[i][j] != A[j][i]) 
+			{
+                flag = 0;
+                break;
+            }
+        }
+        if (flag == 0)
+            break;
+    }
+
+   
+    if (flag == 1)
+        printf("\nThe matrix is symmetric.\n");
+    else
+        printf("\nThe matrix is not symmetric.\n");
+
+    return 0;
+}
+
