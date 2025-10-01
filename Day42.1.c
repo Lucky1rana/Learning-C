38ee213dc170fe41513e9c54b0979be9bcef6e53
Lucky Rana <luckyranaluckyrana995@gmail.com>
2025-10-01 08:43:15 +0530

    Add files via upload

---
+++ Day42.1.c
@@ -0,0 +1,35 @@
+/*Count vowels and consonants in a string.*/
+
+
+#include <stdio.h>
+#include <ctype.h> 
+
+int main() 
+{
+    char str[100];
+    int vowels = 0, consonants = 0, i = 0;
+
+    printf("Enter a string: ");
+    gets(str);   
+
+    while (str[i] != '\0') 
+	{
+        char ch = tolower(str[i]);  
+
+        if (ch >= 'a' && ch <= 'z') 
+		{  
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                vowels++;
+            else
+                consonants++;
+        }
+        i++;
+    }
+
+    printf("Number of vowels: %d\n", vowels);
+    printf("Number of consonants: %d\n", consonants);
+
+    return 0;
+}
+
+
