1638a5f0eab8198fc88d602b4752177049801921
Vansh Kumar <sji694496@gmail.com>
2025-10-03 11:38:37 +0530

    Update Day44.2.c

--- Day44.2.c
+++ Day44.2.c
@@ -9,13 +9,13 @@ int main()
     int i;
 
     printf("Enter a string: ");
-    gets(str); // simple for beginners (but unsafe in real use)
+    gets(str);
 
     for (i = 0; str[i] != '\0'; i++) 
 	{
         if (str[i] == ' ') 
 		{
-            str[i] = '-';  // replace space with hyphen
+            str[i] = '-';  
         }
     }
 
