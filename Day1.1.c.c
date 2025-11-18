c18f157a22c3b28a71ab5341000321bed902b9ec
Vansh Kumar <sji694496@gmail.com>
2025-11-18 17:40:20 +0530

    Refactor sum program for improved clarity
    Refactor the sum program for better readability and consistency.

--- Day1.1.c.c
+++ Day1.1.c.c
@@ -1,15 +1,13 @@
 /*Write a program to input two numbers and display their sum.*/
 
 #include<stdio.h>
-int main()
-{
+int main(){
 	int a , b, sum;
 	printf("Enter the two numbers ");
-	scanf("%d%d",&a,&b);
-	
+	scanf("%d %d",&a,&b);
 	sum = a + b;
-	
 	printf("the sum of two numbers is %d",sum);
-	return 0;
-}
+	return 0; 
+} 
+
 
