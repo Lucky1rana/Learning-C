36459d72164d833bd5813a4faa69c603dc771b40
Lucky Rana <luckyranaluckyrana995@gmail.com>
2025-11-19 11:13:35 +0530

    Add files via upload

---
+++ Day68.c.c
@@ -0,0 +1,31 @@
+/*Write a program to take an input array of size n. The array should contain all the integers between 0 to n except for one. Print that missing number*/
+
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    int i;
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int sum = 0;
+    for(i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    int total = n * (n + 1) / 2;  
+    int missing = total - sum;
+
+    printf("Missing number is: %d\n", missing);
+
+    return 0;
+}
+
